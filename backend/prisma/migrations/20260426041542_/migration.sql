b3c8ab2b7b6c78d7db950534d98a8c02ebc8632e
Andrey <andreykosmenuk13@gmail.com>
2026-04-26 06:19:08 +0200

    fix

---
+++ backend/prisma/migrations/20260426041542_/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Merchant" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "cityId" TEXT NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
+    "rules" TEXT NOT NULL,
+
+    CONSTRAINT "Merchant_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Merchant_cityId_idx" ON "Merchant"("cityId");
